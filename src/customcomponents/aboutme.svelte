77e2d8d9ac170c916fc27d18b59b46d995c32b11
Jeremy <91553695+DonCarbone1234@users.noreply.github.com>
2024-04-10 16:27:11 -0500

    thigns and stuff

---
+++ src/customcomponents/aboutme.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+export var imagesource: string;
+export var header: string;
+export var description: string;
+</script>
+
+<style>
+    html{
+        margin: 0;
+    }
+    div{
+        min-width: 200px;
+        width: min-content;
+        min-height: 300px;
+        height: min-content;
+        padding: 5% 5% 5% 5%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background-color: #59515E;
+        border-radius: 10px;
+        align-items: center;
+    }
+    .header {
+        font-size: 30px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: 600;
+        margin-top: 25%;
+        margin-bottom: 10%;
+    }
+    .description {
+        font-size:20px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+
+<body>
+    <div class="aboutmewrapper">
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src={imagesource} alt="About me Image"/>
+        <span class="header">{header}</span>
+        <span class="description">{description}</span>
+    </div>
+</body>
--- src/customcomponents/home.svelte
+++ src/customcomponents/home.svelte
@@ -1,5 +1,5 @@
-<script>
-
+<script lang="ts">
+    import Aboutme from "./aboutme.svelte";
 </script>
 
 <style>
@@ -8,8 +8,45 @@
         width: 100%;
         height: 100%;
     }
+    .aboutmesection{
+        width: 100%;
+        height: 100%;
+        /* padding-top: 5%; */
+        padding-bottom: 5%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center !important;
+        gap: 10px;
+    }
+    .titlesection {
+        height: min-content;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 </style>
 
 <html lang="en">
     <p>homepage</p>
+    <div class="titlesection">
+        <h1>About Us</h1>
+    </div>
+    <div class="aboutmesection">
+        <Aboutme
+        imagesource="https://d1l3jc4magixw.cloudfront.net/cases/2008/NA_2008_2451/2008_NA_2008_2451_hero_1.jpg"
+        header="Jeremy"
+        description="awesome"
+        />
+        <Aboutme
+        imagesource="https://d1l3jc4magixw.cloudfront.net/cases/2008/NA_2008_2451/2008_NA_2008_2451_hero_1.jpg"
+        header="Nicholas"
+        description="awesome"
+        />
+        <Aboutme
+        imagesource="https://d1l3jc4magixw.cloudfront.net/cases/2008/NA_2008_2451/2008_NA_2008_2451_hero_1.jpg"
+        header="Ronin"
+        description="awesome"
+        />
+    </div>
 </html>
