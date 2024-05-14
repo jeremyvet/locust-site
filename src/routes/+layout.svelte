a9f42e338c1f0c7d6445bd370a7e7d4327089784
Jeremy <91553695+DonCarbone1234@users.noreply.github.com>
2024-05-14 10:45:51 -0500

    historical precedent page done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,8 @@ import ProcedurePage from "../customcomponents/procedure.svelte";
 import HomePage from "../customcomponents/home.svelte";
 import HistoricalPrecedentPage from "../customcomponents/historical-precedent.svelte";
 import SolutionPage from "../customcomponents/solution.svelte";
-import EvidencePage from "../customcomponents/evidence.svelte";
-import ArtPage from "../customcomponents/art.svelte";
+// import EvidencePage from "../customcomponents/evidence.svelte";
+// import ArtPage from "../customcomponents/art.svelte";
 </script>
 
 <html class="dark" lang="en">
@@ -15,13 +15,13 @@ import ArtPage from "../customcomponents/art.svelte";
         <!-- Not best practice - uses tab list to switch between pages, each page is a component -->
         <nav>
             <Tabs.Root value="HomeTab" class="w-[90%]">
-                <Tabs.List class="grid w-full grid-cols-6">
+                <Tabs.List class="grid w-full grid-cols-4">
                     <Tabs.Trigger value="HomeTab">Home</Tabs.Trigger>
                     <Tabs.Trigger value="historical-precedent">Historical Precedent</Tabs.Trigger>
                     <Tabs.Trigger value="procedure">Procedure</Tabs.Trigger>
                     <Tabs.Trigger value="solution">Solution</Tabs.Trigger>
-                    <Tabs.Trigger value="evidence">Evidence</Tabs.Trigger>
-                    <Tabs.Trigger value="art">Art</Tabs.Trigger>
+                    <!-- <Tabs.Trigger value="evidence">Evidence</Tabs.Trigger> -->
+                    <!-- <Tabs.Trigger value="art">Art</Tabs.Trigger> -->
                 </Tabs.List>
                 <Tabs.Content value="HomeTab">
                     <Card.Content class="flex justify-center w-full h-auto">
@@ -59,7 +59,7 @@ import ArtPage from "../customcomponents/art.svelte";
                         </div>
                     </Card.Content>
                 </Tabs.Content>
-                <Tabs.Content value="evidence">
+                <!-- <Tabs.Content value="evidence">
                     <Card.Content class="flex justify-center w-full h-auto">
                         <div class="flex items-center justify-center w-11/12 h-11/12">
                             <Card.Root value="ProcedureCard" class="w-full h-full">
@@ -67,8 +67,8 @@ import ArtPage from "../customcomponents/art.svelte";
                             </Card.Root>
                         </div>
                     </Card.Content>
-                </Tabs.Content>
-                <Tabs.Content value="art">
+                </Tabs.Content> -->
+                <!-- <Tabs.Content value="art">
                     <Card.Content class="flex justify-center w-full h-auto">
                         <div class="flex items-center justify-center w-11/12 h-11/12">
                             <Card.Root value="ProcedureCard" class="w-full h-full">
@@ -76,7 +76,7 @@ import ArtPage from "../customcomponents/art.svelte";
                             </Card.Root>
                         </div>
                     </Card.Content>
-                </Tabs.Content>
+                </Tabs.Content> -->
                 
             </Tabs.Root>
         </nav>
